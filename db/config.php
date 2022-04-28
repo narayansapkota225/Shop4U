f8b7c1c592a81a79512f4e45d120117bdb8cf871
Alpin <alpinchewandy@gmail.com>
2022-04-28 10:02:29 +0000

    test

--- db/config.php
+++ db/config.php
@@ -1,15 +1,12 @@
 <?php
-/* conect to DB*/
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'phpadmin');
-define('DB_PASSWORD', 'FedMates2022@');
-define('DB_NAME', 'Shop4U');
- 
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+$sname= "localhost";
+$unmae= "phpadmin";
+$password = "FedMates2022@";
+$db_name = "Shop4U";
+
+$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+if (!$conn) {
+
+    echo "Connection failed!";
+
 }
-?>
