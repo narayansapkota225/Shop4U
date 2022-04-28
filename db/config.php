91c219ec710bbd4a48af027b337e8a19121438df
narayansapkota225 <narayan225@outlook.com>
2022-04-28 20:02:25 +0930

    Enter

--- db/config.php
+++ db/config.php
@@ -12,4 +12,5 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+echo "Connection Was Successful From Remote Server"
 ?>
