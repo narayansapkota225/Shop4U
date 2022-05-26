225fc3f02add50b7d90e2923c4069dc9e611d8b5
Alpin <alpinchewandy@gmail.com>
2022-05-26 22:24:33 +0930

    Removed test messages

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php $Title = "User Page - Shop4U"?>
-<?php include('partial/adminmenu.php')?>
-<!-- content here -->
-<form class="d-flex justify-content-end" action="" method="POST"> 
-        <input class="form-control me-2" type="text" placeholder="Search" name="search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-<!-- content end-->
-<?php include('partial/footer.php')?>
