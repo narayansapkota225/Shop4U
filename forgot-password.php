c1788f3b0e3f8b80194bbbaf5be70636a3b877da
Alpin <alpinchewandy@gmail.com>
2022-10-12 13:08:02 +1030

    Error messages optimised for better design

--- forgot-password.php
+++ forgot-password.php
@@ -1,17 +1,27 @@
 <?php $Title = "Forgot Password - Shop4U"?>
 <?php include('partial/regis.php')?>
 <!-- reset password start here -->
-<div class="container-fluid">
+<div class="container">
     <div>
         <div class="rounded d-flex justify-content-center">
             <div class="shadow-lg p-5 bg-light" style="margin-top:50px margin-bottom:50px;">
                 <div class="text-center">
                     <h3 class="text-primary">Recover Your Password</h3>
                 </div>
-                <p> An e-mail will be sent to you with instructions on how to reset your password.</p>
+                <?php
+            if (isset($_GET["reset"])) {
+                if ($_GET["reset"] == "success") {
+                    echo '<p class="alert alert-success alert-dismissible fade show">A password reset request has been successfully sent to your email. Please check you email to proceed ahead.</p>';
+                } elseif ($_GET['reset'] == "noaccount") {
+                    $message = '<p class="alert alert-danger alert-dismissible fade show m-2"><i class="bi bi-exclamation-triangle-fill"></i> An account with this email address doesn&apos;t exist. Please create an account first! <a href="signup.php">Sign Up for an Account</a><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></p>';
+                    echo $message;
+
+                }
+            }
+            ?>
                 <form class="row g-3 needs-validation" action="reset-request.php" method="post">
                     <div class="col-md-10 center">
-                        <label class="form-label">Username or Email</label>
+                        <h5 class="form-label text-lg">Username or Email</h5>
                         <input type="text" name="email" class="form-control" placeholder="Enter your email address.." required>
                         <div class="invalid-feedback">
                             Please provide a valid Email.
@@ -21,13 +31,6 @@
                     <button class="btn btn-primary" type="submit">Send Email</button>
                 </div>
             </form>
-            <?php
-            if (isset($_GET["reset"])) {
-                if ($_GET["reset"] == "success") {
-                    echo '<p class="alert alert-success alert-dismissible fade show">A password reset request has been successfully sent to your email. Please check you email to proceed ahead.</p>';
-                }
-            }
-            ?>
             </div>
         </div>  
     </div>
